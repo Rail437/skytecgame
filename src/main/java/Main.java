4c65e89367e5fafc097ccf2d22e4b8fc08ef6888
RailYunusov <opel808437@gmail.com>
2022-10-10 12:13:54 +0300

    10.10.22 12:13 Очередь отрабатывает корректно. Запись в БД в несколько потоков. Добавлена проверка кланов при старте.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,31 +13,33 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class Main {
     public static void main(String[] args) {
         JDBCConnection jdbcConnection = new PostgresJDBCConnection();
-        QueueManager manager = new QueueManager(new ClanJDBCUtility(jdbcConnection) ,new TransactionRepo(jdbcConnection));
+        QueueManager manager = new QueueManager(new ClanJDBCUtility(jdbcConnection), new TransactionRepo(jdbcConnection));
         ClanController controller = new ClanController(manager);
         ExecutorService executorService = Executors.newFixedThreadPool(65);
         ExecutorService executor = Executors.newFixedThreadPool(2);
         manager.checkClanMap();
-        executor.submit(()->manager.start());
+        executor.submit(() -> manager.start());
         for (int i = 0; i < 64; i++) {
-            Long finalI = Long.valueOf(i+1);
-            executorService.submit(()-> run(controller, finalI));
+            Long finalI = Long.valueOf(i + 1);
+            executorService.submit(() -> run(controller, finalI));
         }
 //        manager.removeAllClans();
 //        manager.removeAllTransactions();
-        executor.submit(()->manager.stop());
+        executor.submit(() -> manager.stop());
         executorService.shutdown();
         executor.shutdown();
     }
 
 
-    private static void run(ClanController controller, Long i){
+    private static void run(ClanController controller, Long i) {
         Clan clan = controller.getClan(i);
-//        Clan clan = controller.saveClan(new Clan(Thread.currentThread().getName(), new AtomicInteger(0)));
-        for (int j = 0; j < 1000; j++) {
+        if (clan == null) {
+        clan = controller.saveClan(new Clan(i,Thread.currentThread().getName(), new AtomicInteger(0)));
+        }
+        for (int j = 0; j < 1005; j++) {
             controller.clanTaskPlus(clan.getId());
         }
-        for (int j = 0; j < 1001; j++) {
+        for (int j = 0; j < 1000; j++) {
             controller.clanTaskMinus(clan.getId());
         }
 
